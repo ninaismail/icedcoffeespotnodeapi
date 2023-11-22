4dbd1a3fda5e818b4cf5b69af82d404333b2912d
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2023-11-22 12:22:25 +0200

    fixed the order post api and added the right same req header name for the token to the verifytoken function

--- routes/VerifyToken.js
+++ routes/VerifyToken.js
@@ -1,18 +1,33 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
+  const authHeader = req.headers.authorization;
+
   if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
-      next();
-    });
+    const tokenParts = authHeader.split(" ");
+    
+    if (tokenParts.length === 2 && tokenParts[0] === "Bearer") {
+      const token = tokenParts[1];
+
+      jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+        if (err) {
+          return res.status(403).json({ error: "Token is not valid!" });
+        }
+
+        // Attach the user object to the request for further middleware or route handling
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).json({ error: "Invalid token format!" });
+    }
   } else {
-    return res.status(401).json("You are not authenticated!");
+    return res.status(401).json({ error: "Authorization header is missing!" });
   }
 };
+
+module.exports = verifyToken;
+
 
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
--- routes/order.js
+++ routes/order.js
@@ -12,18 +12,26 @@ router.post('/', verifyToken, async (req, res) => {
   const errors = [];
 
   if (!req.body.address) {
-    errors.push({message:"Address is required."});
+    errors.push({ message: "Address is required." });
   }
-  
+
+  if (errors.length > 0) {
+    // If there are validation errors, send a 400 Bad Request response
+    return res.status(400).json({ errors });
+  }
+
   const newUser = new Order(req.body);
-  
-    try {
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
-    } catch (err) {
-      res.status(500).json(err);
+
+  try {
+    const savedUser = await newUser.save();
+    // Send a 201 Created response with the saved user data
+    res.status(201).json(savedUser);
+  } catch (err) {
+    // Send a 500 Internal Server Error response with the error details
+    res.status(500).json({ error: err.message });
   }
-})
+});
+
     
 //GET USER ORDERS
 router.get("/find/:user_id", verifyTokenAndAuthorization, async (req, res) => {
