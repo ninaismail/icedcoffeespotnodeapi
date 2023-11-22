29d1b527a0ccfd9c3f03e94509547ae58fbfa4eb
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2023-11-22 08:42:54 +0200

    fixed the headers problem on the frontend

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
   const usernameExists = await User.findOne({ username: req.body.username });
   const emailExists = await User.findOne({ email: req.body.email });
   const phoneExists = await User.findOne({ phone: req.body.phone });
-  
+
   if (usernameExists) {
     errors.push({ message: "Username already exists" });
   }
@@ -24,95 +24,95 @@ router.post("/register", async (req, res) => {
   }
 
   if (!req.body.name) {
-    errors.push({message:"Name is required."});
+    errors.push({ message: "Name is required." });
   }
 
   if (!req.body.username) {
-    errors.push({message:"Username is required."});
-  } 
-  
-  if (!req.body.email) {
-    errors.push({message:"Email is required."});
-  } else if (!req.body.email.includes("@")) {
-    errors.push({message:"Email is invalid."});
-  } 
-  
-  if (!req.body.password) {
-    errors.push({message:"Password is required."});
-  } else if (req.body.password.length < 8) {
-    errors.push({message:"Password is invalid."});
+    errors.push({ message: "Username is required." });
   }
-  
+
+  if (!req.body.email) {
+    errors.push({ message: "Email is required." });
+  } else if (!req.body.email.includes("@")) {
+    errors.push({ message: "Email is invalid." });
+  }
+
+  if (!req.body.password) {
+    errors.push({ message: "Password is required." });
+  } else if (req.body.password.length < 8) {
+    errors.push({ message: "Password is invalid." });
+  }
+
   if (!req.body.phone) {
-    errors.push({message:"Phone is required."});
+    errors.push({ message: "Phone is required." });
   } else if (req.body.phone.length < 8) {
-    errors.push({message:"Phone is invalid."});
+    errors.push({ message: "Phone is invalid." });
   }
 
   if (errors.length > 0) {
-    res.status(400).json({ errors });
+    return res.status(400).json({ errors });
   }
-  
-    const newUser = new User({
-      name: req.body.name,
-      username: req.body.username,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.SEC_PASS_KEY
-      ).toString(),
-    });
-  
+
+  const newUser = new User({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SEC_PASS_KEY
+    ).toString(),
+  });
+
   try {
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
-  
-
-//LOGIN
-router.post('/login', async (req, res) => {
-  try{
-      const user = await User.findOne(
-          {
-              email: req.body.email
-          }
-      );
-
-      !user && res.status(401).json("Wrong Email");
-
-      const hashedPassword = CryptoJS.AES.decrypt(
-          user.password,
-          process.env.SEC_PASS_KEY
-      );
-
-
-      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-      const inputPassword = req.body.password;
-      
-      originalPassword != inputPassword && 
-          res.status(401).json("Wrong Password");
-      // for example if this user wants to delete a user we check if it's id is in the token and if he is an admn so he can do that 
-      const accessToken = jwt.sign(
-      {
-          id: user._id,
-          isAdmin: user.isAdmin,
-      },
-      // this means that after 3days we can not access the use token anymore so the user has to login again
-      process.env.JWT_SEC,
-          {expiresIn:"3d"}
-      );
-      //we shouldnt show hashed password to anyone
-      const { password, ...others } = user._doc;  
-      res.status(200).json({...others, accessToken});
-
-  }catch(err){
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(401).json("Wrong Email");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SEC_PASS_KEY
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    const inputPassword = req.body.password;
+
+    if (originalPassword !== inputPassword) {
+      return res.status(401).json("Wrong Password");
+    }
+
+    // for example if this user wants to delete a user we check if it's id is in the token and if he is an admn so he can do that 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      // this means that after 3 days we can not access the use token anymore so the user has to login again
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    // we should not show the hashed password to anyone
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err.message || "Internal Server Error");
+  }
+});
+
 
 module.exports = router;
